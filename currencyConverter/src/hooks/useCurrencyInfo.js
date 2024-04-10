7577c52cb52a9a2105cb1da6fe7a297b1a41d939
Your Name <soniyagupta82270@gmail.com>
2024-04-10 16:15:26 +0530

    add UI

---
+++ currencyConverter/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,19 @@
+import {useState, useEffect} from 'react';
+
+const useCurrencyInfo = () => {
+    const [data, setData] = useState('USD');
+
+    useEffect(() => {
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024.4.10/v1/currencies/${currency}.json`)
+        .then(response => response.json())
+        .then(res => {
+            setData(res[currency]);
+        })
+    },[currency]);
+    console.log(data);
+    return data;
+}
+
+export default useCurrencyInfo;
+
+
